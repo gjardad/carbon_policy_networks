515432872b9df8952accebec22501209de16f669
gjardad <88840944+gjardad@users.noreply.github.com>
2024-07-09 18:54:01 +0200

    Add files via upload

---
+++ installation_year.R
@@ -0,0 +1,18 @@
+#### HEADER -------
+
+## This code creates data set at the installation-year level with info on
+# 1. emissions
+# 2. BvD id
+# 3. activity and NACE ids
+
+#####################
+
+## Import data ------
+
+df_installation <- 1
+
+df_account <- 1
+
+df_compliance <- 1
+
+
