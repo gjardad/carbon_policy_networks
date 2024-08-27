f45022bfab7651b198c4324cabc35e082eaa1eb4
gjardad <88840944+gjardad@users.noreply.github.com>
2024-08-27 15:46:57 -0500

    makes activity ids consistent
    adjusts activity ids such that they are consitent between phases I-II and phase III. mapping obtained in table C1 in Abrell's documentation for EUETS.INFO.

--- installation_year.R
+++ installation_year.R
@@ -56,7 +56,19 @@ installation_year_emissions <- df_installation %>%
   filter(year <= 2023) %>% 
   mutate(allocatedFree = ifelse(is.na(allocatedFree),0, allocatedFree),
          allocatedTotal = ifelse(is.na(allocatedTotal),0, allocatedTotal),
-         verified = ifelse(is.na(verified),0, verified))
+         verified = ifelse(is.na(verified),0, verified)) %>% 
+  # make activity_ids consistent
+    # in the first two phases of EUETS installations were classified into 1-10 activity ids
+    # in phase 3 onwards, installations were classified into 20-99
+    # crosswalk between the two is in Table C1 in Abrell's documentation of EUETS.INFO
+  mutate(activity_id = case_when(
+    activity_id >= 1 & activity_id <= 5 ~ activity_id + 19,
+    activity_id == 6 ~ 29, # ambiguous: activity 6 could become 29 or 30
+    activity_id == 7 ~ 31,
+    activity_id == 8 ~ 32,
+    activity_id == 9 ~ 35, # ambiguous: activity 9 could become 35 or 36
+    TRUE ~ activity_id  # Leave unchanged if it doesn't match any condition
+  ))
 
 ## Save it ------
 save(installation_year_emissions, file = paste0(proc_data,"/installation_year_emissions.RData"))
