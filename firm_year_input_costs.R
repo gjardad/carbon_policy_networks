da1273003c7ccbfbdcaad79eedf930c55544b66e
gjardad <88840944+gjardad@users.noreply.github.com>
2024-08-29 14:08:59 -0500

    just created .R file

---
+++ firm_year_input_costs.R
@@ -0,0 +1,30 @@
+#### HEADER -------
+
+## This code creates data set with firm-year total input costs
+
+#####################
+
+
+# Setup ------
+rm(list = ls())
+
+if(Sys.info()[["user"]] =="JARDANG"){
+  folder <- "X:/Documents/JARDANG" 
+}
+
+raw_data <- paste0(folder, "/carbon_policy_networks/data/raw")
+
+int_data <- paste0(folder, "/carbon_policy_networks/data/intermediate")
+
+proc_data <- paste0(folder, "/carbon_policy_networks/data/processed")
+
+output <- paste0(folder, "/carbon_policy_networks/output")
+
+code <- paste0(folder, "/carbon_policy_networks/code")
+
+# Libraries ----
+
+library(tidyverse)
+library(dplyr) # even though dplyr is included in tidyverse, still need to load it separately
+
+# Import data ------
