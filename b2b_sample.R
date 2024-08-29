ab7ae0f8e446de8159ef5348e0bbcc119535c10d
gjardad <88840944+gjardad@users.noreply.github.com>
2024-08-29 14:09:20 -0500

    just created file

---
+++ b2b_sample.R
@@ -0,0 +1,32 @@
+#### HEADER -------
+
+## This code creates data set that is random sample of B2B
+
+#####################
+
+
+# Setup ------
+rm(list = ls())
+
+if(Sys.info()[["user"]] =="JARDANG"){
+  folder <- "X:/Documents/JARDANG" 
+}
+
+raw_data <- paste0(folder, "/carbon_policy_networks/data/raw")
+
+int_data <- paste0(folder, "/carbon_policy_networks/data/intermediate")
+
+proc_data <- paste0(folder, "/carbon_policy_networks/data/processed")
+
+output <- paste0(folder, "/carbon_policy_networks/output")
+
+code <- paste0(folder, "/carbon_policy_networks/code")
+
+# Libraries ----
+
+library(tidyverse)
+library(dplyr) # even though dplyr is included in tidyverse, still need to load it separately
+
+# Import data ------
+
+df_b2b <- 
