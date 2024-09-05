a531b68f39dcc6bd0cc49428d13df12ac9673ebf
gjardad <88840944+gjardad@users.noreply.github.com>
2024-09-05 16:27:45 -0500

    created data set

--- firm_activity_year.R
+++ firm_activity_year.R
@@ -26,10 +26,37 @@ code <- paste0(folder, "/carbon_policy_networks/code")
 
 library(tidyverse)
 library(dplyr) # even though dplyr is included in tidyverse, still need to load it separately
-library(summarytools)
 
-# Import data ----
+# Import data ------
 
-df_installation <- load(paste0(proc_data,"/installation_year_emissions.RData"))
+load(paste0(proc_data,"/installation_year_emissions.RData"))
 
-load(paste0(proc_data,"/firm_year_emissions.RData"))
+df_account <- read.csv(paste0(raw_data,"/EUTL/account.csv"))
+
+# Clean data ------
+
+df_account <- df_account %>% 
+  rename(account_id = id, account_type = accountType_id, bvd_id = bvdId,
+         firm_id = companyRegistrationNumber) %>% 
+  select(account_id, account_type, bvd_id, installation_id, firm_id) %>% 
+  filter(account_type %in% c("100-7","120-0"))
+
+merged_data <- merge(installation_year_emissions, df_account, by = "installation_id")
+
+firm_activity_year_emissions <- merged_data %>%
+  group_by(firm_id, activity_id, year, country_id) %>%
+  summarize(activity_emissions = sum(verified, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  filter(firm_id != "")
+
+firm_year_total_emissions <- firm_activity_year_emissions %>%
+  group_by(firm_id, year, country_id) %>%
+  summarize(total_emissions = sum(activity_emissions, na.rm = TRUE))
+
+firm_activity_year_emissions <- firm_activity_year_emissions %>% 
+  left_join(firm_year_total_emissions,
+            by = c("firm_id", "year", "country_id")) %>% 
+  mutate(share_emissions = ifelse(total_emissions == 0, 0, activity_emissions/total_emissions))
+
+# Save it -------
+save(firm_activity_year_emissions, file = paste0(proc_data,"/firm_activity_year_emissions.RData"))
