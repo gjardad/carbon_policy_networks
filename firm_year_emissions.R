8993211cf2967d01b50505ccb2830911c4f09e29
gjardad <88840944+gjardad@users.noreply.github.com>
2024-10-28 13:17:44 -0500

    get firms' activities

--- firm_year_emissions.R
+++ firm_year_emissions.R
@@ -8,6 +8,15 @@
 # 5. number of allowances allocated for free
 # 6. number of total allowances allocated
 
+# Obs:
+
+# 1. firm_id and bvd_id are not 1:1 matching. There are more distinct firm_ids than
+# there are bvd_ids. This is because there are some firm_ids which contain "0" in front and some that
+# don't, even in cases when the underlying identifier is the same
+# (e.g. firm_ids 0419052173 and 419052173 are matched with the same bvd_id)
+# also, there are multiple firm_ids for which bvd_id is missing (and therefore belongs to the category
+# of firms for which firm_ids are different but bvd_id is the same, in particular bvd_id == "")
+
 #####################
 
 ## Setup ------
@@ -34,7 +43,7 @@ library(dplyr) # even though dplyr is included in tidyverse, still need to load 
 
 # Import data ------
 
-df_installation <- load(paste0(proc_data,"/installation_year_emissions.RData"))
+load(paste0(proc_data,"/installation_year_emissions.RData"))
 
 df_account <- read.csv(paste0(raw_data,"/EUTL/account.csv"))
 
@@ -48,17 +57,19 @@ df_account <- df_account %>%
 
 firm_year_emissions  <- installation_year_emissions %>% 
   left_join(df_account, by = "installation_id") %>% 
-  group_by(firm_id, year) %>%
+  group_by(bvd_id, year) %>%
   summarise(
     allocated_free = sum(allocatedFree, na.rm = T),
     allocated_total = sum(allocatedTotal, na.rm = T),
     emissions = sum(verified, na.rm = TRUE),  # Total verified emissions
     bvd_id = first(bvd_id),  # Include bvd_id (constant within each group)
-    country_id = first(country_id)  # Include country_id (constant within each group)
-    # obs: country_id and bvd_id are only inconsistent for two firms
+    country_id = first(country_id),  # Include country_id (constant within each group)
+    
+    # activity_id associated with the installation with the largest verified emissions
+    activity_id = activity_id[which.max(verified)]  
   ) %>%
   ungroup() %>% 
-  filter(firm_id != "")
+  filter(bvd_id != "")
 
 # Save it -------
 save(firm_year_emissions, file = paste0(proc_data,"/firm_year_emissions.RData"))
