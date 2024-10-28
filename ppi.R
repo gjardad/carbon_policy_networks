ebd45fdb640aa650f51c943bd43d541733d48086
gjardad <88840944+gjardad@users.noreply.github.com>
2024-10-28 12:44:35 -0500

    binds ippi, sppi, cppi in a single df

--- ppi.R
+++ ppi.R
@@ -1,12 +1,9 @@
 #### HEADER -------
 
-## This code cleans the PPI Belgian data from Eurostat
-
-# It produces a data set at the level of the nace code-year with the PPI index
-# using 2010 as base year (2010 = 100)
+## This code creates a producer prices index at the sector-year level for services,
+# industry and construction
 
 #####################
-
 
 ## Setup ------
 rm(list = ls())
@@ -28,255 +25,140 @@ code <- paste0(folder, "/carbon_policy_networks/code")
 # Libraries ----
 
 library(tidyverse)
-library(dplyr) # even though dplyr is included in tidyverse, still need to load it separately
+library(dplyr)
+
+# Import data ----
+
+load(paste0(proc_data,"/ippi_final.RData"))
+
+load(paste0(proc_data,"/sppi_final.RData"))
+
+load(paste0(proc_data,"/cppi_final.RData"))
+
+load(paste0(proc_data,"/nace_hicp_data.RData"))
+
 library(readxl)
+nace_codes <- read_excel(paste0(raw_data, "/list_nace_codes.xlsx")) %>% 
+  slice(-c(1:5)) %>%
+  select(2) %>% 
+  rename(code = 1) %>% 
+  mutate(code = as.numeric(code)) %>% 
+  mutate(across(where(is.numeric), round, digits = 2)) %>% 
+  mutate(code = as.character(code)) %>% 
+  mutate(
+    code = case_when(
+      str_detect(code, "^\\d\\.") ~ str_pad(code, width = nchar(code) + 1, pad = "0", side = "left"),  # Add zero for 1 digit before "."
+      !str_detect(code, "\\.") & nchar(code) == 1 ~ paste0("0", code),  # Add zero if no "." and only one character
+      TRUE ~ code  # Keep as is if it doesn't meet the other conditions
+    ),
+    code = str_replace_all(code, "\\.", "")
+  )
 
-# Create ppi by sector-year -----
+nace_codes <- nace_codes %>% 
+  filter(!is.na(code))
 
-  # Initialize an empty data frame to store results
-  ppi_data <- data.frame()
+# Merge ppis to nace codes
 
-  # Find data sets within raw_data/Eurostat folder
-  file_folder <- paste0(raw_data, "/Eurostat")
-  file_names <- list.files(path = file_folder, pattern = "^eurostat_ppi", full.names = FALSE)
+  ippi_nace <- ippi_final %>%
+    filter(str_detect(code, "^[A-Za-z]\\d+$")) %>%  # ^[A-Za-z] ensures the first character is a letter, \\d+ checks for following numbers
+    mutate(code = str_replace(code, "^[A-Za-z]", "")) %>% 
+    rename(price_index = ippi_final)
+
+  sppi_nace <- sppi_final %>% 
+    filter(str_detect(code, "^[A-Za-z]\\d+$")) %>%  # ^[A-Za-z] ensures the first character is a letter, \\d+ checks for following numbers
+    mutate(code = str_replace(code, "^[A-Za-z]", "")) %>% 
+    rename(price_index = sppi_final)
   
-  # Loop through each data set
-  for (file in file_names){
-    
-    file_path <- paste0(filde_folder,"/", file)
-    
-    # Get the names of all sheets
-    sheet_names <- excel_sheets(file_path)
-    sheet_names <- sheet_names[3:length(sheet_names)] # no data in first two
-    
-    # Loop through each sheet within data set
-    for (sheet in sheet_names) {
-      
-      # Read the necessary cells from the sheet
-      sector <- read_excel(file_path, sheet = sheet, range = "C7", col_names = FALSE)[[1]]
-      sector_code <- sub(".*\\[(.*)\\].*", "\\1", sector)
-      index_year <- substr(read_excel(file_path, sheet = sheet, range = "C9", col_names = FALSE)[[1]], 8, 11)
-      years <- seq(2000, 2023)  # Years from 2000 to 2023
-      price_indices <- read_excel(file_path, sheet = sheet, range = "C13:AX13", col_names = FALSE)
-      price_indices <- price_indices[, seq(1, ncol(price_indices), by = 2)]
-      
-      # Reshape the data and append to the main data frame
-      temp_df <- data.frame(
-        code = sector_code,
-        year = years,
-        index_year = index_year,
-        price_index = as.numeric(price_indices)
-      )
-      
-      # Append to the main dataset
-      ppi_data <- rbind(ppi_data, temp_df)
-    }
-  }
+  # simply stack them because the codes in each one of them are mutually excludent
+  price_indices <- rbind(ippi_nace, sppi_nace, cppi)
   
-  #save(ppi_data, file = paste0(int_data,"/ppi_data.RData"))
-
-  # Reshape the data to wide format with columns for each index year
-  library(tidyr)
-  ppi_wide <- pivot_wider(ppi_data, 
-                          names_from = index_year, 
-                          values_from = price_index,
-                          names_prefix = "index_",
-                          values_fill = list(price_index = NA))
-
-  # Transform all three indices into 2010 = 100 index and create "collapsed" index
-  ppi_wide <- ppi_wide %>% 
-    group_by(code) %>% 
+  ppi <- nace_codes %>%
+    expand_grid(year = 2005:2022)
+  
+  # populate price_index of nace codes using 
+  ppi <- ppi %>% 
+    left_join(price_indices, by=c("code", "year"))
+  
+  # if disaggregated NACE has no correspondence with price_indices, but parent code does,
+  # then populate using parent code
+  
+  # 3-digit parent codes for 4-digit NACE codes, and 2-digit parent codes for 3-digit NACE codes
+  ppi <- ppi %>% 
+    mutate(parent1 = str_sub(code, 1, -2)) %>%
+    left_join(price_indices, by = c("parent1" = "code", "year"), suffix = c("", ".parent1")) %>%
+    mutate(price_index = ifelse(is.na(price_index), price_index.parent1, price_index)) %>%  # Fill NA with the new price_index
+    select(-price_index.parent1)
+  
+  # 2-digit parent codes for 4-digit NACE codes
+  ppi <- ppi %>% 
+    mutate(parent2 = ifelse(nchar(code) == 4, str_sub(code, 1, 2), NA)) %>%
+    left_join(price_indices, by = c("parent2" = "code", "year"), suffix = c("", ".parent2")) %>%
+    mutate(price_index = ifelse(is.na(price_index), price_index.parent2, price_index)) %>%  # Fill NA with the new price_index
+    select(-price_index.parent2)
+  
+  ppi <- ppi %>% 
+    select(-c(parent1, parent2))
+  
+  ppi <- ppi %>% 
+    distinct()
+  
+  # impute price_index in 2005 for codes for which it is missing
+  ppi <- ppi %>%
+    group_by(code) %>%
     mutate(
-      # make sure its numeric
-      index_2015 = as.numeric(index_2015),
-      index_2021 = as.numeric(index_2021),
-      
-      # Get the index value for the year 2010 for each index
-      index_2015_value_in_10 = index_2015[year == 2010],
-      index_2021_value_in_10 = index_2021[year == 2010],
-    
-      # Transform index_2015 and index_2021 relative to 2010
-      index_2015 = ifelse(!is.na(index_2015_value_in_10), 100 / index_2015_value_in_10 * index_2015, NA),
-      index_2021 = ifelse(!is.na(index_2021_value_in_10), 100 / index_2021_value_in_10 * index_2021, NA),
-      
-      # Collapse into one column
-      ppi_2010 = coalesce(index_2010, index_2015, index_2021)
+      # Step 2: Calculate percentual variation between 2006 and 2007
+      perc_variation = ifelse(year == 2006 & !is.na(price_index), 
+                              (lead(price_index) - price_index) / price_index, 
+                              NA)
+    ) %>%
+    # Step 3: For the rows where price_index is missing in 2005, calculate the imputed value
+    mutate(
+      price_index = ifelse(year == 2005 & is.na(price_index),
+                           price_index[year == 2006] / (1 + perc_variation[year == 2006]),
+                           price_index)
     ) %>%
     ungroup() %>% 
-    select(c(code, year, ppi_2010))
+    select(-perc_variation)
   
-  #save(ppi_wide, file = paste0(int_data,"/ppi_wide.RData"))
+  # add data from hicp for sectors for which ppi not available
   
-# Create most disaggregated ppi by sector-year
+  # nace_hicp only includes 4-digit NACE codes, so this connects 4-digit NACE in ppi
+  # with 4-digit NACE in nace_hicp
+  ppi <- ppi %>% 
+    left_join(nace_hicp_data %>% select(nace, year, price_index),
+              by = c("code" = "nace", "year"), suffix = c("", ".hicp")) %>% 
+    mutate(price_index = ifelse(is.na(price_index), price_index.hicp, price_index)) %>% 
+    select(-price_index.hicp)
   
-  # step 1: identify codes
-  codes <- ppi_wide %>%  distinct(code)
+  # some 4-digit NACE in ppi still have no price_index
+  # for those, calculate average among sectors
+    # 1. that share same first 3/2 digits; and
+    # 2. for which we do have price_index
   
-  # step 2: identify "unorthodox" parent codes
-  library(stringr)
-  unorthodox_codes <- ppi_wide %>% 
-    group_by(code) %>% 
-    slice(1) %>% 
-    select(code) %>% 
-    filter(str_detect(code, "-|_")) %>% 
-    rename(parent_code = code)
-  
-  # step 3: build parent codes
-  
-    # step 3.1: parent_code1
-    parent_codes <- codes %>%
-    mutate(parent_code1 = NA_character_) %>%
-    rowwise() %>%
-    mutate(
-      parent_code1 = case_when(
-        # For codes that are letter + 4 digits (e.g., B1234), take letter + 3 digits
-        str_detect(code, "^[BCD]\\d{4}$") ~ str_sub(code, 1, 4),
-        
-        # For codes that are letter + 3 digits (e.g., B123), take letter + 2 digits
-        str_detect(code, "^[BCD]\\d{3}$") ~ str_sub(code, 1, 3),
-        
-        # For B + 2 digits (e.g., B12), parent_code1 is just B
-        str_detect(code, "^B\\d{2}$") ~ "B",
-        
-        # For D + 2 digits (e.g., D12), parent_code1 is "B-D"
-        str_detect(code, "^D\\d{2}$") ~ "B-D",
-        
-        # For C + specific 2 digits, parent_code1 is from parent_codes and contains "_" and "CXX"
-        str_detect(code, "^C(10|11|13|14|17|18|20|21|22|23|24|25|26|27|29|30|31|32)$") ~ {
-          match <- unorthodox_codes %>% 
-            filter(str_detect(parent_code, "_") & str_detect(parent_code, str_c("C", str_sub(code, 2, 3)))) %>%
-            pull(parent_code)
-          ifelse(length(match) > 0, match[1], NA_character_)
-        },
-        
-        # For C + other specific 2 digits, parent_code1 is from parent_codes and contains "-" and "CXX"
-        str_detect(code, "^C(12|15|16|33)$") ~ {
-          match <- unorthodox_codes %>% 
-            filter(str_detect(parent_code, "-") & str_detect(parent_code, str_c("C", str_sub(code, 2, 3)))) %>%
-            pull(parent_code)
-          ifelse(length(match) > 0, match[1], NA_character_)
-        },
-        
-        # For codes starting with "C" and containing "_", parent_code1 is from parent_codes and contains "-" and first three elements of code
-        str_detect(code, "^C(10|13|31)_.+") ~ {
-          match <- unorthodox_codes %>% 
-            filter(str_detect(parent_code, "-") & str_detect(parent_code, str_sub(code, 1, 3))) %>%
-            pull(parent_code)
-          ifelse(length(match) > 0, match[1], NA_character_)
-        },
-        
-        # For codes starting with "C" + 17, 20, 22, 24, 26, or 29 and containing "_", parent_code1 is "C"
-        # (those are included in thw category above, but parent_codes is empty for those)
-        str_detect(code, "^C(17|20|22|24|26|29)_.+") ~ "C",
-        
-        # For codes starting with "C" and containing "-", parent_code1 is "B_C"
-        str_detect(code, "^C.*-") ~ "B_C",
-        
-        # Default case
-        TRUE ~ NA_character_
-      )
-    ) %>%
+  nace_hicp_3d <- nace_hicp_data %>% 
+    mutate(parent1 = str_sub(nace, 1, 3)) %>% 
+    group_by(parent1, year) %>% 
+    summarize(price_index = mean(price_index, na.rm = TRUE)) %>% 
     ungroup()
-    
-    # step 3.2: add parent_code1 by hand
-    parent_codes[2,2] <- "B-E36"
-    parent_codes[3,2] <- "B-E36"
-    parent_codes[12,2] <- "B-D"
-    parent_codes[13,2] <- "B_C"
-    parent_codes[14,2] <- "B_C_X_MIG_NRG"
-    parent_codes[15,2] <- "B_C"
-    parent_codes[33,2] <- "B_C"
-    parent_codes[136,2] <- "C"
-    parent_codes[275,2] <- "C"
-    parent_codes[349,2] <- "B-D"
-    parent_codes[354,2] <- "B-E36"
-    parent_codes[c(71:77),2] <- "C11"
   
-    # step 3.3: parent_code2
-    parent_codes <- parent_codes %>%
-      left_join(parent_codes %>% select(code, parent_code1), 
-                by = c("parent_code1" = "code"), 
-                suffix = c("", "_parent")) %>%
-      mutate(parent_code2 = parent_code1_parent) %>%
-      select(-parent_code1_parent) # Clean up by removing the extra column
-    
-    # step 3.4: parent_code3
-    parent_codes <- parent_codes %>%
-      left_join(parent_codes %>% select(code, parent_code1), 
-                by = c("parent_code2" = "code"), 
-                suffix = c("", "_parent")) %>%
-      mutate(parent_code3 = parent_code1_parent) %>%
-      select(-parent_code1_parent) 
-    
-    # step 3.5: parent_code4
-    parent_codes <- parent_codes %>%
-      left_join(parent_codes %>% select(code, parent_code1), 
-                by = c("parent_code3" = "code"), 
-                suffix = c("", "_parent")) %>%
-      mutate(parent_code4 = parent_code1_parent) %>%
-      select(-parent_code1_parent) # Clean up by removing the extra column
-    
-    # step 3.6: parent_code5
-    parent_codes <- parent_codes %>%
-      left_join(parent_codes %>% select(code, parent_code1), 
-                by = c("parent_code4" = "code"), 
-                suffix = c("", "_parent")) %>%
-      mutate(parent_code5 = parent_code1_parent) %>%
-      select(-parent_code1_parent) # Clean up by removing the extra column
-    
-    # step 3.7: parent_code6
-    parent_codes <- parent_codes %>%
-      left_join(parent_codes %>% select(code, parent_code1), 
-                by = c("parent_code5" = "code"), 
-                suffix = c("", "_parent")) %>%
-      mutate(parent_code6 = parent_code1_parent) %>%
-      select(-parent_code1_parent) # Clean up by removing the extra column
-    
-    # step 3.8: parent_code7
-    parent_codes <- parent_codes %>%
-      left_join(parent_codes %>% select(code, parent_code1), 
-                by = c("parent_code6" = "code"), 
-                suffix = c("", "_parent")) %>%
-      mutate(parent_code7 = parent_code1_parent) %>%
-      select(-parent_code1_parent) # Clean up by removing the extra column
-    
-  parent_codes <- parent_codes[-c(4:11),]
+  ppi <- ppi %>% 
+    mutate(parent1 = ifelse(nchar(code) == 4, str_sub(code, 1, 3), NA)) %>% 
+    left_join(nace_hicp_3d, by = c("parent1", "year"), suffix = c("", ".parent1")) %>%
+    mutate(price_index = ifelse(is.na(price_index), price_index.parent1, price_index)) %>%  # Fill NA with the new price_index
+    select(-c(price_index.parent1, parent1))
   
-  #save(parent_codes, file = paste0(int_data,"/parent_codes.RData"))
-    
-  # step 4: create ppi_final
-    
-    # step 4.1: create ppi_final in ppi_wide
-    ppi_wide$ppi_final <- ppi_wide$ppi_2010
-    
-    # step 4.2: loop over rows with missing ppi_2010
-    missing_rows <- which(is.na(ppi_wide$ppi_2010))
-    
-    for (i in missing_rows) {
-      code <- ppi_wide$code[i]
-      year <- ppi_wide$year[i]
-      
-      # Find all parent codes for this code
-      parent_candidates <- parent_codes[parent_codes$code == code, ]
-      
-      # Iterate over the parent_code variables (parent_code1, parent_code2, ..., parent_code7)
-      for (j in 1:7) {
-        parent_code <- parent_candidates[[paste0("parent_code", j)]]
-        
-        # Check if the parent_code has a non-missing ppi_2010 value in the same year
-        ppi_value <- ppi_wide$ppi_2010[ppi_wide$code == parent_code & ppi_wide$year == year]
-        
-        if (length(ppi_value) > 0 && !is.na(ppi_value)) {
-          # Assign the first non-missing ppi_2010 from the parent code to ppi_final
-          ppi_wide$ppi_final[i] <- ppi_value
-          break
-        }
-      }
-    }
+  nace_hicp_2d <- nace_hicp_data %>% 
+    mutate(parent2 = str_sub(nace, 1, 2)) %>% 
+    group_by(parent2, year) %>% 
+    summarize(price_index = mean(price_index, na.rm = TRUE)) %>% 
+    ungroup()
   
-    # step 4.3: clean and save ppi_wide
-    ppi_final <- ppi_wide %>% 
-      select(-c(ppi_2010))
+  ppi <- ppi %>% 
+    mutate(parent2 = ifelse(nchar(code) == 4, str_sub(code, 1, 2), NA)) %>% 
+    left_join(nace_hicp_2d, by = c("parent2", "year"), suffix = c("", ".parent2")) %>%
+    mutate(price_index = ifelse(is.na(price_index), price_index.parent2, price_index)) %>%  # Fill NA with the new price_index
+    select(-c(price_index.parent2, parent2))
   
-    save(ppi_final, file = paste0(proc_data,"/ppi_final.RData"))
+# save it ------
+  save(ppi, file = paste0(proc_data,"/ppi_final.RData"))
   
