a52a47af56278cc63990b3e2fa3e46eb90907ebb
gjardad <88840944+gjardad@users.noreply.github.com>
2024-11-01 15:03:49 -0500

    firm-year-level inputs from euets firms

---
+++ euets_inputs.R
@@ -0,0 +1,10 @@
+#### HEADER -------
+
+## This code creates firm-year-level measures of EUETS inputs
+
+# 1. total expenditure share on EUETS treated inputs
+# 2. expenditure share on each of the EUETS inputs
+# 3. expenditure share on each of the emission intensive inputs
+# (regardless of whether the supplier is EUETS or not)
+
+#####################
