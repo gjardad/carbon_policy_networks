92a7ede2118d5f17df61891c13aab647b5bc7917
gjardad <88840944+gjardad@users.noreply.github.com>
2024-11-01 15:02:43 -0500

    firm-year-level measure of network exposure

---
+++ network_exposure.R
@@ -0,0 +1,11 @@
+#### HEADER -------
+
+## This code creates firm-year-level measures of network exposure to the carbon shock
+
+# 1. downstream exposure_it = \sum_j x_{ijt} (1+allowance shortage_{jt})
+# 2. upstream exposure_it = \sum_j x_{jit} (1+allowance shortage_{jt})
+
+#####################
+
+
+
