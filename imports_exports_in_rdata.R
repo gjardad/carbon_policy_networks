ee395de459459bdbbab9dd812ee5f433e9f49560
gjardad <88840944+gjardad@users.noreply.github.com>
2025-02-03 15:17:54 -0600

    created .RData version of trade data

---
+++ imports_exports_in_rdata.R
@@ -0,0 +1,35 @@
+#### HEADER -------
+
+## Save customs dataset in .RData format
+
+#####################
+
+## Setup ------
+rm(list = ls())
+
+if(Sys.info()[["user"]] =="JARDANG"){
+  folder <- "X:/Documents/JARDANG" 
+}
+
+raw_data <- paste0(folder, "/carbon_policy_networks/data/raw")
+
+int_data <- paste0(folder, "/carbon_policy_networks/data/intermediate")
+
+proc_data <- paste0(folder, "/carbon_policy_networks/data/processed")
+
+output <- paste0(folder, "/carbon_policy_networks/output")
+
+code <- paste0(folder, "/carbon_policy_networks/code")
+
+# Libraries ----
+
+library(tidyverse)
+library(dplyr) # even though dplyr is included in tidyverse, still need to load it separately
+
+# Import data ----
+
+library(haven)
+df_trade <- read_dta(paste0(raw_data,"/NBB/import_export_ANO.dta"))
+
+# Save it ----
+save(df_trade, file = paste0(proc_data,"/df_trade.RData"))
