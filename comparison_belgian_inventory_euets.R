17ed1f5d34bb87d417c8e0b2dd81d46d31d5c983
gjardad <88840944+gjardad@users.noreply.github.com>
2025-04-11 17:12:40 -0500

    matched ctf categ. to ets activities

--- comparison_belgian_inventory_euets.R
+++ comparison_belgian_inventory_euets.R
@@ -1,6 +1,6 @@
 #### HEADER -------
 
-## Compare sectoral emissions with Belgian national inventory
+## Compare ETS emissions with Belgian national inventory
 
 #####################
 
@@ -29,31 +29,57 @@ library(readxl)
 
 # Import data -----
 
-load(paste0(proc_data,"/firm_year_balance_sheet_selected_sample.RData"))
-load(paste0(proc_data,"/firm_year_belgian_euets.RData"))
-load(paste0(output,"/sector_year_emissions_euets_noneuets.RData"))
-inventory <- read_excel(paste0(raw_data, "/BE_2024_Art14_AnnexXII_Consistency_with_ETS_280224.xlsx"))
-crf_to_nace <- read_excel(paste0(raw_data, "/Annex-I-(Correspondence-between-CRF-NFR-NACE-Rev.-2)-to-Manual-for-Air-Emissions-Accounts-(2015-edition).xlsx"),
-                          sheet = "Correspondence-CRF-NFR-NACE")
+crf_to_activities_crosswalk <- read_excel(paste0(raw_data, "/NIR/crf_to_ets_activities.xlsx")) %>%
+  mutate(crf_code = sub(" .*", "", crf_category))
+
+inventory <- read_excel(paste0(raw_data, "/NIR/BE_2024_Art14_AnnexXII_Consistency_with_ETS_280224.xlsx"))
+
+load(paste0(proc_data, "/installation_year_emissions.RData"))
 
 # Clean data -----
 
-colnames(inventory) <- c("sector","inventory", "ets", "ratio", "comment")
+be_installations_2022 <- installation_year_emissions %>% 
+  filter(country_id == "BE", year == 2022) %>% 
+  group_by(activity_id, year) %>% 
+  summarise(emissions = sum(verified)) %>% 
+  mutate(activity_id = as.character((activity_id)),
+         emissions = emissions/10^6) %>% # emissions measured in mi of tons
+  ungroup()
+
+# split installations with activity id 20 into multiple categories based on NACE
+be_installations_2022_act20 <- installation_year_emissions %>% 
+  filter(country_id == "BE", year == 2022, activity_id == 20) %>% 
+  mutate(nace_id = ifelse(str_detect(nace_id, "^[0-9]{1}\\."), 
+                          str_replace(nace_id, "^", "0"),
+                          nace_id), # add zero to the left
+         nace2d = substr(nace_id, 1, 2)) %>% 
+  group_by(nace2d) %>% 
+  summarise(emissions = sum(verified)) %>% 
+  mutate(activity_id = "20",
+         emissions = emissions/10^6) %>% # emissions measured in mi of tons
+  ungroup()
+
+
+crf_expanded <- crf_to_activities_crosswalk %>%
+  mutate(activities = strsplit(as.character(activity), ", ")) %>%
+  unnest(activities) %>%
+  left_join(be_installations_2022, by = c("activities" = "activity_id")) %>% 
+  rename(ets_emissions = emissions)
+
+colnames(inventory) <- c("crf_category","inventory_emissions", "ets_emissions_in_inventory", "ratio", "comment")
 inventory <- inventory[-c(1:14, 74:nrow(inventory)),-c(4:5)]
 inventory <- inventory %>% 
-  mutate(sector_name = str_remove(sector, "^[^ ]+ "),
-         sector_code = str_extract(sector, "^[^ ]+"),
+  mutate(crf_name = str_remove(crf_category, "^[^ ]+ "),
+         crf_code = str_extract(crf_category, "^[^ ]+"),
          across(c(2,3), ~ as.numeric(.x)/10^3)) %>% # emissions measured in mi of tons
-  filter(!is.na(sector)) %>% 
-  select(-sector) %>% 
-  select(sector_name, sector_code, inventory, ets)
+  filter(!is.na(crf_category)) %>% 
+  select(-crf_category) %>% 
+  select(crf_name, crf_code, inventory_emissions, ets_emissions_in_inventory) %>% 
+  mutate(crf_code = paste0(crf_code, "."))
 
-crf_to_nace <- crf_to_nace[, c(9,12,15,16)]
-colnames(crf_to_nace) <- c("crf_before_2015", "crf_after_2015", "nace_rev2", "comment")
-crf_to_nace <- crf_to_nace %>% 
-  filter(!is.na(crf_before_2015), nace_rev2 != "-")
-crf_to_nace <- crf_to_nace[-c(1),]
+  # Emissions by CRF category
+  crf_to_activities_emissions <- crf_expanded %>% 
+    left_join(inventory %>% select(crf_code, inventory_emissions, ets_emissions_in_inventory),
+              by = "crf_code")
 
-
-# Compare national accounts NACE codes with EUETS NACE codes (from EUETS.INFO)
 
