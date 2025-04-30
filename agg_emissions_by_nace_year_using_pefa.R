d938b16935d1dd22cc48073a0ec6467349e29f66
gjardad <88840944+gjardad@users.noreply.github.com>
2025-04-30 18:41:13 -0500

    changed nace D to D35

--- agg_emissions_by_nace_year_using_pefa.R
+++ agg_emissions_by_nace_year_using_pefa.R
@@ -37,7 +37,8 @@ agg_emissions_by_nace_year_using_pefa <- emissions_from_energy_by_nace_year %>%
   rename(emissions_from_energy = emissions) %>% 
   left_join(emissions_from_ind_process_by_nace_year, by = c("year", "nace")) %>% 
   mutate(agg_emissions = replace_na(emissions_from_energy, 0) + replace_na(emissions, 0)) %>% 
-  select(-c(emissions_from_energy, emissions))
+  select(-c(emissions_from_energy, emissions)) %>% 
+  mutate(nace = ifelse(nace == "D", "D35", nace)) # make it comparable to others datasets
 
 # save it
 # save(agg_emissions_by_nace_year_using_pefa, file = paste0(proc_data, "/agg_emissions_by_nace_year_using_pefa.RData"))
