7c9e062b1153d5062beefbeadc82ccba848189d7
gjardad <88840944+gjardad@users.noreply.github.com>
2025-04-30 18:40:53 -0500

    made nace code comparable to eurostat

--- euets_emissions_by_nace_year.R
+++ euets_emissions_by_nace_year.R
@@ -51,9 +51,63 @@ euets_emissions_by_nace_using_firms <- firm_year_belgian_euets %>%
   group_by(nace2d, year) %>% 
   summarise(emissions = sum(emissions, na.rm = T))
 
+# Make NACE sectors comparable ------
+
+euets_emissions_by_nace_using_firms <- euets_emissions_by_nace_using_firms %>% 
+  rename(nace = nace2d) %>% 
+  mutate(nace = case_when(
+    nace %in% c("07", "08") ~ "B",                 
+    nace %in% c("10", "11", "12") ~ "C10-C12",    
+    nace %in% c("13", "14", "15") ~ "C13-C15",    
+    nace %in% c("16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "33") ~ paste0("C", nace),  
+    nace == "35" ~ "D35",
+    nace == "36" ~ "E36",
+    nace %in% c("37", "38", "39") ~ "E37-E39",
+    nace %in% c("41", "42", "43") ~ "F",
+    nace %in% c("45", "46", "47") ~ paste0("G", nace), 
+    nace %in% c("49", "50", "51", "52", "53") ~ paste0("H", nace),
+    nace == "63" ~ "J62_J63", # no firms classified as 62,
+    nace == "64" ~ "K64",
+    nace == "68" ~ "L68A",
+    nace == "70" ~"M69_M70",
+    nace %in% c("71", "72") ~ paste0("M", nace),
+    nace %in% c("81", "82") ~ "N80-N82",
+    nace == "84" ~ "O",
+    nace == "86" ~"Q86",
+    TRUE ~ nace                                   
+  )) %>%
+  group_by(nace, year) %>%                        
+  summarise(emissions = sum(emissions, na.rm = TRUE), .groups = "drop")  # Summarise emissions
+
+euets_emissions_by_nace_using_installation <- euets_emissions_by_nace_using_installation %>% 
+  rename(nace = nace2d) %>% 
+  mutate(nace = case_when(
+    nace %in% c("07", "08") ~ "B",                 
+    nace %in% c("10", "11", "12") ~ "C10-C12",    
+    nace %in% c("13", "14", "15") ~ "C13-C15",    
+    nace %in% c("16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "33") ~ paste0("C", nace),  
+    nace == "35" ~ "D35",
+    nace == "36" ~ "E36",
+    nace %in% c("37", "38", "39") ~ "E37-E39",
+    nace %in% c("41", "42", "43") ~ "F",
+    nace %in% c("45", "46", "47") ~ paste0("G", nace), 
+    nace %in% c("49", "50", "51", "52", "53") ~ paste0("H", nace),
+    nace == "63" ~ "J62_J63", # no firms classified as 62,
+    nace == "64" ~ "K64",
+    nace == "68" ~ "L68A",
+    nace == "70" ~"M69_M70",
+    nace %in% c("71", "72") ~ paste0("M", nace),
+    nace %in% c("81", "82") ~ "N80-N82",
+    nace == "84" ~ "O",
+    nace == "86" ~"Q86",
+    TRUE ~ nace                                   
+  )) %>%
+  group_by(nace, year) %>%                        
+  summarise(emissions = sum(emissions, na.rm = TRUE), .groups = "drop")  # Summarise emissions
+
 # save it
-#save(euets_emissions_by_nace_using_firms, file = paste0(proc_data, "/euets_emissions_by_nace_using_firms.RData"))
-#save(euets_emissions_by_nace_using_installation, file = paste0(proc_data, "/euets_emissions_by_nace_using_installation.RData"))
+save(euets_emissions_by_nace_using_firms, file = paste0(proc_data, "/euets_emissions_by_nace_using_firms.RData"))
+save(euets_emissions_by_nace_using_installation, file = paste0(proc_data, "/euets_emissions_by_nace_using_installation.RData"))
 
 
 
