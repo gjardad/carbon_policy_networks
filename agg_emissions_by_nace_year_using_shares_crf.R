12eb840391a6b3d692fbf304cc486a906535bf29
gjardad <88840944+gjardad@users.noreply.github.com>
2025-04-30 15:01:21 -0500

    agg emissions using shares crf
    agg emissions by nace year using shares of national inventories emissions proportional to fuel consumption (for emissions from energy) and to value added (for emissions from industrial process).

---
+++ agg_emissions_by_nace_year_using_shares_crf.R
@@ -0,0 +1,45 @@
+#### HEADER -------
+
+## Creates time series of agg. emissions by NACE sector using shares of CRF
+
+#####################
+
+## Setup ------
+rm(list = ls())
+
+if(Sys.info()[["user"]] =="JARDANG"){
+  folder <- "X:/Documents/JARDANG" 
+}
+
+raw_data <- paste0(folder, "/carbon_policy_networks/data/raw")
+
+int_data <- paste0(folder, "/carbon_policy_networks/data/intermediate")
+
+proc_data <- paste0(folder, "/carbon_policy_networks/data/processed")
+
+output <- paste0(folder, "/carbon_policy_networks/output")
+
+code <- paste0(folder, "/carbon_policy_networks/code")
+
+# Libraries ----
+
+library(tidyverse)
+library(dplyr)
+library(readxl)
+
+# Import data -----
+
+load(paste0(proc_data, "/emissions_from_energy_by_nace_year_using_shares_crf.RData"))
+load(paste0(proc_data, "/emissions_from_ind_process_by_nace_year.RData"))
+
+# Clean data -----
+
+agg_emissions_by_nace_year_using_shares_crf <- emissions_from_energy_by_nace_year %>%
+  rename(emissions_from_energy = emissions) %>% 
+  left_join(emissions_from_ind_process_by_nace_year, by = c("year", "nace")) %>% 
+  mutate(agg_emissions = replace_na(emissions_from_energy, 0) + replace_na(emissions, 0)) %>% 
+  select(-c(emissions_from_energy, emissions))
+
+# save it
+# save(agg_emissions_by_nace_year_using_shares_crf, file = paste0(proc_data, "/agg_emissions_by_nace_year_using_shares_crf.RData"))
+
