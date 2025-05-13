fab52e1213f3156fcf7731e715b37360506693ae
gjardad <88840944+gjardad@users.noreply.github.com>
2025-05-13 17:59:08 -0500

    some changes to the graph

--- reg_firm_emissions_on_imputed_using_firm_size.R
+++ reg_firm_emissions_on_imputed_using_firm_size.R
@@ -35,13 +35,11 @@ load(paste0(proc_data, "/firm_year_obs_and_imputed_emissions_using_firm_size.RDa
 # Clean data -----
 
 df <- firm_year_balance_sheet_and_emissions_using_firm_size %>% 
-  select(vat_ano, year, nace2d, turnover, obs_emissions, belongs_to_covered_nace, agg_emissions) %>% 
+  select(vat_ano, year, nace2d, turnover, obs_emissions, imputation_group, agg_emissions) %>% 
   group_by(year, nace2d) %>%
-  mutate(turnover_share = if_else(
-    belongs_to_covered_nace == 1,
-    turnover / sum(turnover[belongs_to_covered_nace == 1], na.rm = TRUE),
-    0
-  )) %>%
+  mutate(turnover_share = case_when(
+    imputation_group == 3 ~ 0,
+    imputation_group %in% c(1,2,4) ~ turnover / sum(turnover[imputation_group %in% c(1,2,4)], na.rm = TRUE))) %>%
   ungroup() %>% 
   mutate(imputed_emissions = turnover_share * agg_emissions) %>% 
   select(obs_emissions, imputed_emissions)
@@ -58,13 +56,20 @@ summary(model)$r.squared
 ggplot(df, aes(x = imputed_emissions, y = obs_emissions)) +
   geom_point(alpha = 0.6) +
   geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray") +
   labs(
-    title = "Observed vs. Imputed Emissions",
+    title = "",
     x = "Imputed Emissions",
     y = "Observed Emissions"
   ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),  # remove grid lines
+    axis.title.x = element_text(margin = margin(t = 10)),
+    axis.title.y = element_text(margin = margin(r = 10))
+  )
   
+ggsave(paste0(output, "/obs_vs_imputed_emissions.png"))
 
 
 
