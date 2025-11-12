97fa1f7d897414f9a83f58b1d28d73964c17f7f0
gjardad <88840944+gjardad@users.noreply.github.com>
2025-11-12 19:13:21 +0100

    identifies pc8 codes of fossil fuel goods

---
+++ pc8_code_of_klenow_goods.R
@@ -0,0 +1,45 @@
+#### HEADER -------
+
+## This code creates data set that provides CPA v.2.1 codes for each
+# good listed as a fossil fuel in Klenow et al (2025)
+
+#####################
+
+# Setup ------
+rm(list = ls())
+
+if(tolower(Sys.info()[["user"]]) == "jardang"){
+  folder <- "X:/Documents/JARDANG" 
+}
+
+raw_data <- paste0(folder, "/carbon_policy_networks/data/raw")
+
+int_data <- paste0(folder, "/carbon_policy_networks/data/intermediate")
+
+proc_data <- paste0(folder, "/carbon_policy_networks/data/processed")
+
+output <- paste0(folder, "/carbon_policy_networks/output")
+
+code <- paste0(folder, "/carbon_policy_networks/code")
+
+# Libraries ----
+
+library(tidyverse)
+library(dplyr) # even though dplyr is included in tidyverse, still need to load it separately
+
+# Import data -------
+
+load(paste0(proc_data,"/cpc_v2_codes_of_klenow_goods.RData"))
+load(paste0(proc_data,"/cpc_v2_to_cpa_v21.RData"))
+
+# Create data set ------
+
+cpa_v21_codes_of_klenow_goods <- cpc_v2_codes_of_klenow_goods %>% 
+  left_join(cpc_v2_to_cpa_v21, by = "cpc_v2_code") %>% 
+  distinct()
+
+# Save it -------
+save(cpa_v21_codes_of_klenow_goods, file = paste0(proc_data,"/cpa_v21_codes_of_klenow_goods.RData"))  
+
+library(haven)  
+write_dta(cpa_v21_codes_of_klenow_goods, paste0(proc_data,"/cpa_v21_codes_of_klenow_goods.dta"))
