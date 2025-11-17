d9da2d39f5fc12704932763defc05e1bc988ab08
gjardad <88840944+gjardad@users.noreply.github.com>
2025-11-17 15:41:15 +0100

    I, X, C by fuel type and year for BE from eurostat

---
+++ energy_balance_by_fuel_year.R
@@ -0,0 +1,32 @@
+#### HEADER -------
+
+## This code creates a df with I, X, and consumption of fuels in Belgium
+# by fuel type and year
+
+#####################
+
+# Setup ------
+rm(list = ls())
+
+if(tolower(Sys.info()[["user"]]) == "jardang"){
+  folder <- "X:/Documents/JARDANG" 
+}
+
+raw_data <- paste0(folder, "/carbon_policy_networks/data/raw")
+
+int_data <- paste0(folder, "/carbon_policy_networks/data/intermediate")
+
+proc_data <- paste0(folder, "/carbon_policy_networks/data/processed")
+
+output <- paste0(folder, "/carbon_policy_networks/output")
+
+code <- paste0(folder, "/carbon_policy_networks/code")
+
+# Libraries ----
+
+library(tidyverse)
+library(dplyr) # even though dplyr is included in tidyverse, still need to load it separately
+
+# Import and clean data --------
+
+  energy_balance 
