2c811b154dde474294f47317d8444eaea630ccc3
gjardad <88840944+gjardad@users.noreply.github.com>
2025-11-18 22:42:04 +0100

    cleaned energy_balance df

--- energy_balance_by_fuel_year.R
+++ energy_balance_by_fuel_year.R
@@ -1,7 +1,10 @@
 #### HEADER -------
 
-## This code creates a df with I, X, and consumption of fuels in Belgium
-# by fuel type and year
+## This code creates a df with I, X, and domestic consumption,
+# and consumption for generation of energy in the industrial sector
+# by fuel-year in in Belgium
+
+# It also creates list of unique SIEC codes present in the energy balance in Belgium
 
 #####################
 
@@ -29,4 +32,31 @@ library(dplyr) # even though dplyr is included in tidyverse, still need to load 
 
 # Import and clean data --------
 
-  energy_balance 
+  energy_balance <- read_csv(paste0(raw_data, "/Eurostat/energy_balance_by_fuel_year.csv")) %>%
+    select(nrg_bal, siec, TIME_PERIOD, OBS_VALUE) %>% 
+    rename(flow = nrg_bal, year = TIME_PERIOD, value = OBS_VALUE) %>%
+    # exclude total, bioenergy, renewables, and waste
+    filter(!substr(siec, 1, 2) %in% c("RA", "TO", "BI", "W6")) %>% 
+    tidyr::separate(
+      siec,
+      into = c("siec_fullcode", "siec_name"),
+      sep = ":", 
+      remove = TRUE
+    ) %>%
+    mutate(
+      siec_code = sub("^[A-Za-z]", "", siec_fullcode),
+      siec_code = sub("_.*$", "", siec_code),
+      value = value %>%
+        gsub("\\.", "", .) %>%     # remove all dots
+        as.numeric() / 1000        # correct decimal placement
+    ) %>% 
+  rename(energy_balance_value = value)
+  # remember: this is in TJ
+
+# List of unique SIEC codes covered in Eurostat energy balance for Belgium ------
+
+unique_siec_eurostat_energy_balance_belgium <- unique(energy_balance$siec_code)
+
+# Save it ------
+save(energy_balance, file = paste0(proc_data,"/energy_balance_by_fuel_year.RData"))
+save(unique_siec_eurostat_energy_balance_belgium, file = paste0(int_data,"/unique_siec_eurostat_energy_balance_belgium.RData"))
