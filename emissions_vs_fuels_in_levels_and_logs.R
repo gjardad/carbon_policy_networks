a4784170fe34e08e1e5f0482fbb3ee0312c03085
gjardad <88840944+gjardad@users.noreply.github.com>
2025-12-09 21:51:36 +0100

    changed axis titles and saved plots

--- emissions_vs_fuels_in_levels_and_logs.R
+++ emissions_vs_fuels_in_levels_and_logs.R
@@ -28,40 +28,86 @@ code <- paste0(folder, "/carbon_policy_networks/code")
 
 library(ggplot2)
 
-# Import data --------
+# Import and clean data --------
 
   load(paste0(proc_data, "/amount_spent_on_fuel_by_firm_year.RData"))
+  
+  load(paste0(proc_data, "/firm_year_belgian_euets.RData"))
+  
+  df <- amount_spent_on_fuel_by_firm_year %>%
+    left_join(firm_year_belgian_euets %>% select(vat, year, revenue),
+              by = c("vat_j_ano" = "vat", "year")
+    ) %>%
+    filter(emissions > 0,
+           !is.na(amount_spent_on_fuel)) %>%
+    mutate(
+      year   = as.factor(year),
+      nace5d = as.factor(nace5d)
+    )
+
+# Drop singleton firms -------
+
+  #obs: there are some sectors with only one firm among the firms with emissions > 0
+  # this means it's not possible to estimate the NACE fixed-effect in the LOFO regs
+  # so we build training data excluding singleton firms
+  
+  sector_firm_counts <- df %>%
+    group_by(nace5d) %>%
+    summarise(n_firms = n_distinct(vat_j_ano), .groups = "drop")
+  
+  valid_sectors <- sector_firm_counts %>%
+    filter(n_firms >= 2) %>%
+    pull(nace5d)
+  
+  df_no_singletons <- df %>% filter(nace5d %in% valid_sectors)
+  
+  df_no_singletons <- df_no_singletons %>%
+    mutate(year = factor(year),
+           nace5d = factor(nace5d))
 
 # Generate plots ------
 
   # Prepare data
-  df_plot <- amount_spent_on_fuel_by_firm_year %>%
-    filter(emissions > 0,
-           amount_spent_on_fuel > 0) %>%  # just in case
+  df_plot <- df_no_singletons %>%
     mutate(
       year   = as.factor(year),
       nace5d = as.factor(nace5d),
       log_em = log(emissions),
-      log_fuel = log(amount_spent_on_fuel))
+      log_fuel = log(amount_spent_on_fuel_excl_euets_importers))
 
   # Plot in levels
-  p1 <- ggplot(df_plot, aes(x = amount_spent_on_fuel, y = emissions)) +
+  p1 <- ggplot(df_plot, aes(x = amount_spent_on_fuel_excl_euets_importers, y = emissions)) +
     geom_point(alpha = 0.3) +
-    #scale_x_continuous(trans = "log10") +
-    #scale_y_continuous(trans = "log10") +
-    labs(title = "Levels: emissions vs fuel",
-         x = "Fuel expenditure",
-         y = "Emissions") +
-    theme_minimal()
-
+    labs(
+      title = "",
+      x = "Fuel Consumption",
+      y = "Emissions"
+    ) +
+    theme_minimal() +
+    theme(
+      axis.title.x = element_text(size = 16, margin = margin(t = 14)),
+      axis.title.y = element_text(size = 16, margin = margin(r = 14)),
+      axis.text    = element_text(size = 14)
+    )
+  
   # Plot in logs
   p2 <- ggplot(df_plot, aes(x = log_fuel, y = log_em)) +
     geom_point(alpha = 0.3) +
-    #geom_smooth(method = "lm", se = FALSE, color = "red") +
-    labs(title = "Logs: log(emissions) vs log(fuel)",
-         x = "log(fuel)",
-         y = "log(emissions)") +
-    theme_minimal()
+    geom_smooth(method = "lm", se = FALSE, color = "red") +
+    labs(
+      title = "",
+      x = "Fuel Consumption",
+      y = "Emissions"
+    ) +
+    theme_minimal() +
+    theme(
+      axis.title.x = element_text(size = 16, margin = margin(t = 14)),
+      axis.title.y = element_text(size = 16, margin = margin(r = 14)),
+      axis.text    = element_text(size = 14)
+    )
   
-  p1
-  p2
+# Save it --------
+ggsave(paste0(output, "/emissions_fuel_levels.png"), p1, width = 8, height = 5, dpi = 300)
+ggsave(paste0(output, "/emissions_fuel_logs.png"), p2, width = 8, height = 5, dpi = 300)
+  
+
